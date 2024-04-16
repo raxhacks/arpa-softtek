ea258c5dae42a11ee6385bba426b7ab774c06067
Checokrispi <105012973+Checokrispi@users.noreply.github.com>
2024-04-16 11:44:18 -0600

    login conection endpoint

--- frontend/app/components/MyTabs.js
+++ frontend/app/components/MyTabs.js
@@ -7,6 +7,7 @@ import user from '@/services/user';
 
 function MyTabs() {
   const [selectedIndex, setSelectedIndex] = useState(0);
+  const [] = useState("");
   //const history = useHistory(); // Obtiene el historial de navegación
 
   // Función para manejar el envío del formulario
@@ -19,6 +20,7 @@ function MyTabs() {
       if (registered) {
         // Si el registro es exitoso, redirige al usuario a la página de inicio de sesión
         history.push('../CargarArchivos/page');
+        console.log("funciono el registro")
       } else {
         console.error('Error al enviar los datos');
       }
@@ -38,9 +40,9 @@ function MyTabs() {
     await sendFormDataToServer(formDataObject);
   };
   return (
-    <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex} className={`mt-20 w-full lg:mt-0 flex flex-grow items-center justify-center`}>
+    <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex} className={`w-full flex flex-grow items-center justify-center`}>
       <Tab.Panels>
-        <Tab.Panel>
+        <Tab.Panel className={`static lg:w-full rounded-lg lg:pb-32 bg-opacity-95`}>
           <Tab></Tab>
           {/* Inicio de Sesion Button */}
           <Tab className={`rounded-lg border border-transparent px-5 py-4 w-full text-center font-semibold text-2xl bg-blue-600 transition-colors hover:border-blue-300 hover:bg-yellow-100 hover:dark:bg-neutral-800/20`}>
@@ -53,7 +55,7 @@ function MyTabs() {
         </Tab.Panel>
 
         {/* Inicio de Sesion tab */}
-        <Tab.Panel className={`w-full lg:static rounded-lg flex-grow grid grid-cols-1 fixed lg:pb-32 pb-72 bg-opacity-95 bg-slate-700`}>
+        <Tab.Panel className={`w-full lg:static rounded-lg flex-grow grid grid-cols-1 fixed lg:pb-0 pb-96 bg-gray-900 bg-opacity-95`}>
           <Tab className={`rounded-lg ml-2 mt-2 border w-16 border-transparent px-5 py-2 text-black`}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -82,7 +84,7 @@ function MyTabs() {
         </Tab.Panel>
         
         {/* Registro tab */}
-        <Tab.Panel className={`w-full lg:static rounded-lg flex-grow grid grid-cols-1 fixed pb-72 lg:pb-32 bg-opacity-95 bg-slate-700`}>
+        <Tab.Panel className={`w-full lg:static rounded-lg flex-grow grid grid-cols-1 fixed pb-96 lg:pb-32 bg-opacity-95 bg-[rgb(77, 80, 97)]`}>
           <Tab className={`rounded-lg ml-2 mt-2 border w-16 border-transparent px-5 py-2 text-black`}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -100,15 +102,27 @@ function MyTabs() {
           <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center mx-auto max-w-sm">
             <label className="block text-center">
               Correo electrónico:<br/>
-              <input name="email" type="email" className="border w-96 border-gray-300 rounded-md px-4 py-2 mt-1 focus:outline-none focus:border-blue-500 text-black" />
+              <input 
+                name="email" 
+                type="email" 
+                className="border w-96 border-gray-300 rounded-md px-4 py-2 mt-1 focus:outline-none focus:border-blue-500 text-black" 
+                placeholder = "Correo electrónico"
+              />
             </label>
             <label className="block mb-2 text-center">
-              Contraseña:<br/>
-              <input name="password" type="password" className="border w-96 border-gray-300 rounded-md px-4 py-2 mt-1 focus:outline-none focus:border-blue-500 text-black" />
+              <input 
+                name="password" 
+                type="password" 
+                className="border w-96 border-gray-300 rounded-md px-4 py-2 mt-1 focus:outline-none focus:border-blue-500 text-black" 
+                placeholder = "Contraseña"
+              />
             </label>
             <label className="block mb-2 text-center">
-              Confirmar contraseña:<br/>
-              <input name="password" type="password" className="border w-full border-gray-300 rounded-md px-4 py-2 mt-1 focus:outline-none focus:border-blue-500 text-black" />
+              <input 
+                type="password" 
+                className="border w-full border-gray-300 rounded-md px-4 py-2 mt-1 focus:outline-none focus:border-blue-500 text-black"
+                placeholder='Confirmar contraseña'
+              />
             </label>
             <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md mb-4 w-full">Enviar</button>
           </form>
