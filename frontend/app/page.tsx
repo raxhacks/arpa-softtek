881d4fe23c73e0ea2e513524677ab17701c4cf3e
Checokrispi <105012973+Checokrispi@users.noreply.github.com>
2024-04-17 12:18:41 -0600

    login final updates

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
               Analiza este articulo
               {/* pr-16 pt-16 flex-col text-5xl justify-center items-center lg:text-6xl pb-5 font-mono */}
             </div>
-            <div className="pr-16 pt-16 lg:max-w-[20ch] flex-col text-3xl h-32 text-center font-mono lg:text-left lg:text-4xl">
+            <div className="pr-16 pt-12 lg:max-w-[20ch] flex-col text-3xl h-32 text-center font-mono lg:text-left lg:text-4xl">
             {/* flex-col text-3xl max-w-[20ch] pt-5 lg:text-4xl font-mono h-32 */}
               <Typewriter
                 options={{
