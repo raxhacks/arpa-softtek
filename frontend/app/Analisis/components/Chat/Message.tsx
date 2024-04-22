57be280e752280b1c91879810d33edf3a06ccf64
Raxhacks <alexhacker654@gmail.com>
2024-04-22 17:06:18 -0600

    add: chat

---
+++ frontend/app/Analisis/components/Chat/Message.tsx
@@ -0,0 +1,25 @@
+type MessageProps = { 
+  message: string;
+  isUser: boolean;
+}
+const Message: React.FC<MessageProps> = ({
+  message,
+  isUser,
+}) => {
+  return (
+    <div className={`w-full inline-flex ${isUser && "justify-end"}`}>
+      <div className={`w-full text-white px-4
+      py-1 rounded-[20px]
+      ${isUser && "bg-[#5756F5] !w-2/3"}`}>
+        {!isUser && 
+          <div className="text-[32px] mb-4">
+            ARPA-Bot
+          </div>
+        }
+        {message}
+      </div>
+    </div>
+  ); 
+};
+
+export default Message;
