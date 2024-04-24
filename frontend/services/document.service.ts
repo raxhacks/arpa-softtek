53783f85d185e24ad5e8491d9a09287d45e4bb62
AlejandroA00833118 <a00833118@itesm.mx>
2024-04-24 09:51:07 -0600

    mover de lugar el servicio de historial

--- frontend/services/document.service.ts
+++ frontend/services/document.service.ts
@@ -2,6 +2,7 @@
 
 import axios from 'axios';
 import { cookies } from 'next/headers';
+import { Document } from '../model/document';
 
 const baseUrl = process.env.NEXT_PUBLIC_API_URL + '/document';
 
@@ -24,3 +25,26 @@ export const createDocument = async (data: FormData, tokenSSR?: string) => {
     }
 };
 
+export const getHistory = async (): Promise<Document[]> => {
+    try {
+        const token = cookies().get('session')?.value
+        const config = { 
+            headers: { 
+                'Authorization': `Bearer ${token}`
+            } 
+        };
+        console.log('Fetching documents history...');
+        const response = await axios.get('https://arpa-2mgft7cefq-uc.a.run.app/history', config);
+
+        const history: Document[] = response.data.map((item: any) => ({
+            id: item.document_id,
+            title: item.title,
+            createdAt: item.created_at
+        }));
+
+        return history;
+    } catch (error) {
+        console.error('Could not fetch history:', error);
+        throw error;
+    }
+};
