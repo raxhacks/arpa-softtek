da25afd0dba8668d0de79f59152adb5ac542c33e
Angel Garcia <102173690+AngelGarcia2207@users.noreply.github.com>
2024-04-30 12:01:39 -0600

    Pasar el header a tailwind y arreglar cosas

--- frontend/app/(main)/header.tsx
+++ frontend/app/(main)/header.tsx
@@ -5,60 +5,58 @@ import { logout } from "../actions/session";
 
 function ARPAHeader() {
     return (
-      <div className="font-semibold ARPA_header z-20">
-        <h2 className="headerText">ARPA</h2>
+      <div className="bg-[#4D5061] text-[#FCFAF5] text-[7vh] font-semibold fixed top-0 h-[20vh] max-h-[75px] left-0 right-0 flex
+      items-center justify-center z-30 md:max-h-[100px] md:justify-between">
+        <h2 className="align-middle md:pl-[5vw]">ARPA</h2>
       </div>
     );
   }
   
   function SectionsHeader() {
     return (
-      <div className="sections_header">
-        <div className="sectionGroup">
-          <div className="sections">
-            <div className="flex flex-row items-center">
+      <div className="bg-[#4D5061] fixed bottom-0 h-[20vh] max-h-[100px] left-0 right-0 flex items-center justify-center z-30
+      md:bg-transparent md:top-0 md:justify-end">
+        <div className="flex flex-row justify-around w-[100%] md:w-auto">
   
-              {/* Boton 1  */}
-              <Link className="mr-4 lg:mr-10" href={`/CargarArchivos`}>
-                <svg xmlns="http://www.w3.org/2000/svg" className="icon-header icon-tabler icon-tabler-news" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FCFAF5" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                  <path d="M16 6h3a1 1 0 0 1 1 1v11a2 2 0 0 1 -4 0v-13a1 1 0 0 0 -1 -1h-10a1 1 0 0 0 -1 1v12a3 3 0 0 0 3 3h11" />
-                  <path d="M8 8l4 0" />
-                  <path d="M8 12l4 0" />
-                  <path d="M8 16l4 0" />
-                </svg>
-              </Link>
-  
-              {/* Boton 2  */}
-              <Link className="mr-4 lg:mr-10" href={`/Favorites`}>
-                <svg xmlns="http://www.w3.org/2000/svg" className="icon-header icon-tabler icon-tabler-bookmarks" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FCFAF5" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                  <path d="M15 10v11l-5 -3l-5 3v-11a3 3 0 0 1 3 -3h4a3 3 0 0 1 3 3z" />
-                  <path d="M11 3h5a3 3 0 0 1 3 3v11" />
-                </svg>
-              </Link>
-  
-              {/* Boton 3  */}
-              <Link className="mr-4 lg:mr-10" href={`/History`}>
-                <svg xmlns="http://www.w3.org/2000/svg" className="icon-header icon-tabler icon-tabler-history" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FCFAF5" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                  <path d="M12 8l0 4l2 2" />
-                  <path d="M3.05 11a9 9 0 1 1 .5 4m-.5 5v-5h5" />
-                </svg>
-              </Link>
+          {/* Boton 1  */}
+          <Link className="md:mr-10" href={`/CargarArchivos`}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon-header icon-tabler icon-tabler-news hover:stroke-[#BBA778]" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FCFAF5" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M16 6h3a1 1 0 0 1 1 1v11a2 2 0 0 1 -4 0v-13a1 1 0 0 0 -1 -1h-10a1 1 0 0 0 -1 1v12a3 3 0 0 0 3 3h11" />
+              <path d="M8 8l4 0" />
+              <path d="M8 12l4 0" />
+              <path d="M8 16l4 0" />
+            </svg>
+          </Link>
 
-              {/* Boton 4  */}
-              <button className="mr-4 lg:mr-10" onClick={() => logout()}>
-                <svg xmlns="http://www.w3.org/2000/svg" className="icon-tabler icon-tabler-logout-2" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FCFAF5" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                  <path d="M10 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" />
-                  <path d="M15 12h-12l3 -3" />
-                  <path d="M6 15l-3 -3" />
-                </svg>
-              </button>         
-  
-            </div>
-          </div>
+          {/* Boton 2  */}
+          <Link className="md:mr-10" href={`/Favorites`}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon-header icon-tabler icon-tabler-bookmarks hover:stroke-[#BBA778]" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FCFAF5" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M15 10v11l-5 -3l-5 3v-11a3 3 0 0 1 3 -3h4a3 3 0 0 1 3 3z" />
+              <path d="M11 3h5a3 3 0 0 1 3 3v11" />
+            </svg>
+          </Link>
+
+          {/* Boton 3  */}
+          <Link className="md:mr-10" href={`/History`}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon-header icon-tabler icon-tabler-history hover:stroke-[#BBA778]" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FCFAF5" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M12 8l0 4l2 2" />
+              <path d="M3.05 11a9 9 0 1 1 .5 4m-.5 5v-5h5" />
+            </svg>
+          </Link>
+
+          {/* Boton 4  */}
+          <button className="md:mr-10" onClick={() => logout()}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon-tabler icon-tabler-logout-2 stroke-[#FAE8BD] hover:stroke-[#BBA778]" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FCFAF5" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M10 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" />
+              <path d="M15 12h-12l3 -3" />
+              <path d="M6 15l-3 -3" />
+            </svg>
+          </button>
+
         </div>
       </div>
     );
