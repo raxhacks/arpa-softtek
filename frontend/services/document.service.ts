075a9a5498b145f8065756078d8612142be72571
Checokrispi <105012973+Checokrispi@users.noreply.github.com>
2024-05-03 14:44:12 -0600

    endpoint error correction

--- frontend/services/document.service.ts
+++ frontend/services/document.service.ts
@@ -16,12 +16,11 @@ export const createDocument = async (data: FormData, tokenSSR?: string) => {
                 'Content-Type': 'multipart/form-data'
             } 
         };
-        console.log('Uploading document...', data);
-        const response = await axios.post('http://127.0.0.1:5001/arpa-softtek/us-central1/arpa/document', data, config);
+        console.log('Uploading document...');
+        const response = await axios.post('https://arpa-2mgft7cefq-uc.a.run.app/document', data, config);
         return response.data;
     } catch (error) {
-        console.error('Could not upload the document:', error);
-        throw error;
+        console.error('Could not upload the document:');
     }
 };
 
