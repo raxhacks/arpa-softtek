8a00c133da513aa7ba0c764af186a7843fcddc88
luisbarajasr <luis.br02@hotmail.com>
2024-05-22 10:55:25 -0600

    unos acentos

--- frontend/app/login/Typewritereffect.js
+++ frontend/app/login/Typewritereffect.js
@@ -1,22 +1,22 @@
 'use client'
 import Typewriter from 'typewriter-effect';
 
-export default function Typewritereffect(){
+export default function Typewritereffect() {
     return (
         <div className="pr-16 pt-16 lg:pt-10 flex-col text-3xl text-center font-mono lg:text-left lg:text-4xl h-32">
-            <Typewriter 
+            <Typewriter
                 options={{
                     strings: [
                         "y dime si habla de la capa de ozono",
                         "y dime si habla de las nebulosas",
-                        "y dime si habla de la guerra fria",
+                        "y dime si habla de la guerra fráa",
                         "y dime si menciona a los mayas",
                         "  ",
-                        "y ayudame a seguir aprendiendo" 
+                        "y ayudame a seguir aprendiendo"
                     ],
                     autoStart: true,
                     loop: true,
-                }}  
+                }}
             />
         </div>
 
