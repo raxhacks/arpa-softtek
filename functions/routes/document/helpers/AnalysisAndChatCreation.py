baf95e304e756bcd2dded562571be08b66983572
raxhacks <alexhacker654@gmail.com>
2024-05-31 09:49:27 -0600

    add: change

--- functions/routes/document/helpers/AnalysisAndChatCreation.py
+++ functions/routes/document/helpers/AnalysisAndChatCreation.py
@@ -72,12 +72,12 @@ def addAnalysisToDocument(user_id, document_id, text, keywords, keywords_str_arr
         time.sleep(1)
 
         retreiver = vectorstore.as_retriever()
-        num_vectors = len(vectorstore.index.vector_count)
-        print(f"Number of vectors in the vector store: {num_vectors}")
+        # num_vectors = len(vectorstore.index.vector_count)
+        # print(f"Number of vectors in the vector store: {num_vectors}")
         
         # print(f"Number of documents in the vector store: {num_docs}")
-        total_tokens = sum(len(doc.pageContent.split()) for doc in vectorstore.query_collection.fetch())
-        print(f"Total number of tokens in the vector store: {total_tokens}")
+        # total_tokens = sum(len(doc.pageContent.split()) for doc in vectorstore.query_collection.fetch())
+        # print(f"Total number of tokens in the vector store: {total_tokens}")
         
         ### Contextualize question ###
         contextualize_q_system_prompt = """Given a document text which can be in english or spanish \
