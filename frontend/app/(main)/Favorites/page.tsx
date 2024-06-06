7e07300339350372089479574d778254f2c0ce21
Checokrispi <105012973+Checokrispi@users.noreply.github.com>
2024-06-06 05:26:22 -0600

    Update page.tsx

--- frontend/app/(main)/Favorites/page.tsx
+++ frontend/app/(main)/Favorites/page.tsx
@@ -159,7 +159,7 @@ export default function MostrarFavoritos() {
                         type="search" 
                         id="default-search" 
                         className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                        placeholder="Buscar dentro del historial..." required 
+                        placeholder="Buscar dentro de favoritos..." required 
                         onChange={(e) => setSearch(e.target.value)}
                     />
                 </div>
